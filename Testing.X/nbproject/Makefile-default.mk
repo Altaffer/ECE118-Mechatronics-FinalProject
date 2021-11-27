d3880a833376f10e1bb22f3ef7cf25bbd96cfe7e
Thomas Luca Pandolfo Altaffer <taltaffe@ucsc.edu>
2021-11-26 18:25:39 -0800

    TestTopLevel

--- Testing.X/nbproject/Makefile-default.mk
+++ Testing.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=C:/ECE118/src/AD.c C:/ECE118/src/BOARD.c C:/ECE118/src/IO_Ports.c C:/ECE118/src/serial.c TestingMain.c ../MurpleCircle.X/robot.c C:/ECE118/src/pwm.c C:/ECE118/src/RC_Servo.c ../MurpleCircle.X/timers.c TestTopLevel.c
+SOURCEFILES_QUOTED_IF_SPACED=C:/ECE118/src/AD.c C:/ECE118/src/BOARD.c C:/ECE118/src/IO_Ports.c C:/ECE118/src/serial.c TestingMain.c ../MurpleCircle.X/robot.c C:/ECE118/src/pwm.c C:/ECE118/src/RC_Servo.c ../MurpleCircle.X/timers.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1975241074/AD.o ${OBJECTDIR}/_ext/1975241074/BOARD.o ${OBJECTDIR}/_ext/1975241074/IO_Ports.o ${OBJECTDIR}/_ext/1975241074/serial.o ${OBJECTDIR}/TestingMain.o ${OBJECTDIR}/_ext/1095528968/robot.o ${OBJECTDIR}/_ext/1975241074/pwm.o ${OBJECTDIR}/_ext/1975241074/RC_Servo.o ${OBJECTDIR}/_ext/1095528968/timers.o ${OBJECTDIR}/TestTopLevel.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1975241074/AD.o.d ${OBJECTDIR}/_ext/1975241074/BOARD.o.d ${OBJECTDIR}/_ext/1975241074/IO_Ports.o.d ${OBJECTDIR}/_ext/1975241074/serial.o.d ${OBJECTDIR}/TestingMain.o.d ${OBJECTDIR}/_ext/1095528968/robot.o.d ${OBJECTDIR}/_ext/1975241074/pwm.o.d ${OBJECTDIR}/_ext/1975241074/RC_Servo.o.d ${OBJECTDIR}/_ext/1095528968/timers.o.d ${OBJECTDIR}/TestTopLevel.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1975241074/AD.o ${OBJECTDIR}/_ext/1975241074/BOARD.o ${OBJECTDIR}/_ext/1975241074/IO_Ports.o ${OBJECTDIR}/_ext/1975241074/serial.o ${OBJECTDIR}/TestingMain.o ${OBJECTDIR}/_ext/1095528968/robot.o ${OBJECTDIR}/_ext/1975241074/pwm.o ${OBJECTDIR}/_ext/1975241074/RC_Servo.o ${OBJECTDIR}/_ext/1095528968/timers.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1975241074/AD.o.d ${OBJECTDIR}/_ext/1975241074/BOARD.o.d ${OBJECTDIR}/_ext/1975241074/IO_Ports.o.d ${OBJECTDIR}/_ext/1975241074/serial.o.d ${OBJECTDIR}/TestingMain.o.d ${OBJECTDIR}/_ext/1095528968/robot.o.d ${OBJECTDIR}/_ext/1975241074/pwm.o.d ${OBJECTDIR}/_ext/1975241074/RC_Servo.o.d ${OBJECTDIR}/_ext/1095528968/timers.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1975241074/AD.o ${OBJECTDIR}/_ext/1975241074/BOARD.o ${OBJECTDIR}/_ext/1975241074/IO_Ports.o ${OBJECTDIR}/_ext/1975241074/serial.o ${OBJECTDIR}/TestingMain.o ${OBJECTDIR}/_ext/1095528968/robot.o ${OBJECTDIR}/_ext/1975241074/pwm.o ${OBJECTDIR}/_ext/1975241074/RC_Servo.o ${OBJECTDIR}/_ext/1095528968/timers.o ${OBJECTDIR}/TestTopLevel.o
+OBJECTFILES=${OBJECTDIR}/_ext/1975241074/AD.o ${OBJECTDIR}/_ext/1975241074/BOARD.o ${OBJECTDIR}/_ext/1975241074/IO_Ports.o ${OBJECTDIR}/_ext/1975241074/serial.o ${OBJECTDIR}/TestingMain.o ${OBJECTDIR}/_ext/1095528968/robot.o ${OBJECTDIR}/_ext/1975241074/pwm.o ${OBJECTDIR}/_ext/1975241074/RC_Servo.o ${OBJECTDIR}/_ext/1095528968/timers.o
 
 # Source Files
-SOURCEFILES=C:/ECE118/src/AD.c C:/ECE118/src/BOARD.c C:/ECE118/src/IO_Ports.c C:/ECE118/src/serial.c TestingMain.c ../MurpleCircle.X/robot.c C:/ECE118/src/pwm.c C:/ECE118/src/RC_Servo.c ../MurpleCircle.X/timers.c TestTopLevel.c
+SOURCEFILES=C:/ECE118/src/AD.c C:/ECE118/src/BOARD.c C:/ECE118/src/IO_Ports.c C:/ECE118/src/serial.c TestingMain.c ../MurpleCircle.X/robot.c C:/ECE118/src/pwm.c C:/ECE118/src/RC_Servo.c ../MurpleCircle.X/timers.c
 
 
 
@@ -161,12 +161,6 @@ ${OBJECTDIR}/_ext/1095528968/timers.o: ../MurpleCircle.X/timers.c  .generated_fi
 	@${RM} ${OBJECTDIR}/_ext/1095528968/timers.o 
 	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG   -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -I"." -I"C:/ECE118/include" -I"../MurpleCircle.X" -MP -MMD -MF "${OBJECTDIR}/_ext/1095528968/timers.o.d" -o ${OBJECTDIR}/_ext/1095528968/timers.o ../MurpleCircle.X/timers.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/TestTopLevel.o: TestTopLevel.c  .generated_files/flags/default/9d2acf02a8ff104b514d6db666e474ce1f66882 .generated_files/flags/default/25cf332145e109ecc94cb8c88ed46e464b66686
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/TestTopLevel.o.d 
-	@${RM} ${OBJECTDIR}/TestTopLevel.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG   -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -I"." -I"C:/ECE118/include" -I"../MurpleCircle.X" -MP -MMD -MF "${OBJECTDIR}/TestTopLevel.o.d" -o ${OBJECTDIR}/TestTopLevel.o TestTopLevel.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
-	
 else
 ${OBJECTDIR}/_ext/1975241074/AD.o: C:/ECE118/src/AD.c  .generated_files/flags/default/c8c74805f14b0089fbddd29e1eca77246552d181 .generated_files/flags/default/25cf332145e109ecc94cb8c88ed46e464b66686
 	@${MKDIR} "${OBJECTDIR}/_ext/1975241074" 
@@ -221,12 +215,6 @@ ${OBJECTDIR}/_ext/1095528968/timers.o: ../MurpleCircle.X/timers.c  .generated_fi
 	@${RM} ${OBJECTDIR}/_ext/1095528968/timers.o.d 
 	@${RM} ${OBJECTDIR}/_ext/1095528968/timers.o 
 	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -I"." -I"C:/ECE118/include" -I"../MurpleCircle.X" -MP -MMD -MF "${OBJECTDIR}/_ext/1095528968/timers.o.d" -o ${OBJECTDIR}/_ext/1095528968/timers.o ../MurpleCircle.X/timers.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
-	
-${OBJECTDIR}/TestTopLevel.o: TestTopLevel.c  .generated_files/flags/default/c950de79547be3962fa36459ba8feba20f988cbf .generated_files/flags/default/25cf332145e109ecc94cb8c88ed46e464b66686
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/TestTopLevel.o.d 
-	@${RM} ${OBJECTDIR}/TestTopLevel.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -I"." -I"C:/ECE118/include" -I"../MurpleCircle.X" -MP -MMD -MF "${OBJECTDIR}/TestTopLevel.o.d" -o ${OBJECTDIR}/TestTopLevel.o TestTopLevel.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
 endif
 
